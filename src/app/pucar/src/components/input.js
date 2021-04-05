d224beeb73b6e1a7a3e5a80000328dc3b45d2673
William de Castro <williamtdcastro@gmail.com>
2021-04-04 23:04:04 -0300

    feat: adding logon page to the app and adding sweetalert

--- src/app/pucar/src/components/input.js
+++ src/app/pucar/src/components/input.js
@@ -1,29 +1,38 @@
-import React, { useEffect, useRef } from 'react'
-import { useField } from '@unform/core'
+import React, { useEffect, useRef } from "react";
+import { useField } from "@unform/core";
 
-import './styles/input.css';
+import "./styles/input.css";
 
 export default function Input({ name, placeholder, styleInput, ...rest }) {
-  const inputRef = useRef(null)
-  const { fieldName, defaultValue, registerField, error } = useField(name)
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, registerField, error } = useField(name);
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRef,
-      getValue: ref => {
-        return ref.current.value
+      getValue: (ref) => {
+        return ref.current.value;
       },
       setValue: (ref, value) => {
-        ref.current.value = value
+        ref.current.value = value;
       },
-      clearValue: ref => {
-        ref.current.value = ''
+      clearValue: (ref) => {
+        ref.current.value = "";
       },
-    })
-  }, [fieldName, registerField])
+    });
+  }, [fieldName, registerField]);
 
   return (
-      <input ref={inputRef} defaultValue={defaultValue} {...rest} className={styleInput} placeholder={placeholder}/>
-  )
+    <>
+      <input
+        ref={inputRef}
+        defaultValue={defaultValue}
+        className={styleInput}
+        placeholder={placeholder}
+        {...rest}
+      />
+      {error && <span className="error">{error}</span>}
+    </>
+  );
 }
--- src/app/pucar/src/routes/routes.tsx
+++ src/app/pucar/src/routes/routes.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Login from '../pages/Login';
+import Logon from "../pages/Logon";
 
 function Routes() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Login} />
+      <Route path="/login" component={Login} />
+      <Route path="/logon" component={Logon} />
     </BrowserRouter>
   );
 }
