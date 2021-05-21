1392172b54d1a13ac1ec2e8dd692c013d2f988ec
William de Castro <williamtdcastro@gmail.com>
2021-05-21 00:33:36 -0300

    fix: arrumando pasta repetida no projeto

--- src/server/server/src/database/index.js
+++
@@ -1,7 +0,0 @@
-const Squelize = require('sequelize');
-
-const dbConfig = require('../config/database');
-
-const connection = new Squelize(dbConfig[process.env.TYPE]);
-
-module.exports = connection;
--- src/server/server/src/routes/register/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const UsuarioController = require('../../controllers/UsuarioController');
-
-const routes = express.Router();
-
-routes.post('/', UsuarioController.store);
-
-module.exports = routes;
