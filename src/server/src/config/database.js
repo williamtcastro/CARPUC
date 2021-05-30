2813fc58aa306a70b89318b3dbaff0749678a7ec
William de Castro <williamtdcastro@gmail.com>
2021-05-29 23:03:58 -0300

    fix: adding pg db to the server

--- src/server/src/config/database.js
+++ src/server/src/config/database.js
@@ -16,11 +16,11 @@ module.exports = {
     port: '3307',
   },
   production: {
-    username: 'root',
+    username: 'docker',
     password: 'docker',
     database: 'pucar',
     host: '127.0.0.1',
-    dialect: 'mysql',
-    port: '3307',
+    dialect: 'postgres',
+    port: '25432',
   },
 };
